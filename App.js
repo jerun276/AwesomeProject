b8dd064425ff6b059637373b26dd175e650b14e1
jerun276 <jerunkingston@gmail.com>
2024-08-15 11:37:51 +0530

    Learned Handling Events

--- App.js
+++ App.js
@@ -1,28 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const[nameText, setNameText] = useState("")
+
+  function goalInputHandler(text) {
+    setNameText(text)
+  }
+
+  function addNameHandler() {
+    console.log(nameText)
+  }
+
   return (
     <View style={styles.appcontainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Enter your name brother: '/>
-        <Button title='Add name'/>
+        <TextInput
+          onChangeText={goalInputHandler}
+          style={styles.textInput}
+          placeholder='Enter your name brother: ' />
+        <Button
+          onPress={addNameHandler}
+          title='Add name' />
       </View>
-      <View>
+      <View style={styles.listContainer}>
         <Text>List of Names</Text>
       </View>
     </View>
-    
+
   );
 }
 
 const styles = StyleSheet.create({
-  appcontainer :{
+  appcontainer: {
     padding: 25,
+    flex: 1,
   },
   inputContainer: {
     flexDirection: 'row',
-    justifyContent:'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    paddingBottom: 25,
+    borderBottomColor: 'green',
+    flex: 1,
   },
   textInput: {
     borderWidth: 1,
@@ -30,6 +52,9 @@ const styles = StyleSheet.create({
     width: '80%',
     marginRight: 10,
     padding: 5,
+  },
+  listContainer: {
+    flex: 4,
   }
 
 });
